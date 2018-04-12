03e0a7bdaf45c86a9d2805f8dbf7fe4fae8e26ae
K1kk3rt <37216006+K1kk3rt@users.noreply.github.com>
2018-04-12 13:12:31 +0200

    Add files via upload

---
+++ week_5/Opdracht 1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+
+namespace Opdracht_1
+{
+
+    public struct Persoon
+    {
+        public string naam;
+        public string woonplaats;
+        public int leeftijd;
+        public bool isBekend;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bestandsNaam = "..\\..\\..\\output.txt";
+
+            Persoon p = LeesNaam(bestandsNaam);
+
+            if (p.isBekend)
+            {
+                ToonPersoon(p, bestandsNaam);
+            }
+            else
+            {
+                p = LeesPersoon(p);
+                SchrijfPersoon(p, bestandsNaam);
+            }
+
+            Console.ReadKey();
+        }
+
+        static Persoon LeesNaam(string bestandsNaam)
+        {
+            Persoon p = new Persoon();
+
+            Console.WriteLine("Naam: ");
+            p.naam = Console.ReadLine();
+
+            //open file
+
+            using (StreamReader reader = File.OpenText(bestandsNaam))
+            {
+                string[] bestand = File.ReadAllLines(bestandsNaam);
+                p.isBekend = false;
+
+                for (int i = 0; i < bestand.Length -1; i++)
+                {
+                    if (p.naam == bestand[i])
+                    {
+                        p.isBekend = true;
+                        Console.WriteLine("Welkom terug! Dit weten we over jou");
+                        break;
+                    }
+                }
+                if (!p.isBekend)
+                {
+                    Console.WriteLine("Ik ken jou niet, vul je shit in!");
+                }
+            }
+            return p;
+        }
+
+        static void ToonPersoon(Persoon p, string bestandsNaam)
+        {
+            using (StreamReader reader = File.OpenText(bestandsNaam))
+            {
+                string[] bestand = File.ReadAllLines(bestandsNaam);
+
+                for (int i = 0; i < bestand.Length - 1; i++)
+                {
+                    if (p.naam == bestand[i])
+                    {
+                        string naam = reader.ReadLine();
+                        Console.Write("Naam: " + naam);
+                        string woonplaats = reader.ReadLine();
+                        Console.Write("Woonplaats: " + woonplaats);
+                        int leeftijd = Int32.Parse(reader.ReadLine());
+                        Console.Write("Leeftijd: " + leeftijd);
+                        break;
+                    }
+                }
+            } 
+        }
+
+        static void SchrijfPersoon (Persoon p, string bestandsNaam)
+        {
+
+            //open file
+            StreamWriter writer = new StreamWriter(bestandsNaam);
+
+            //schrijf alle regels (van de gebruiker) naar de file
+            writer.WriteLine(p.naam);
+            writer.WriteLine(p.woonplaats);
+            writer.WriteLine(p.leeftijd);
+
+            //sluit file
+            writer.Close();
+
+            Console.WriteLine("Opgeslagen!");
+        }
+
+        static Persoon LeesPersoon(Persoon P)
+        {
+            Persoon p = new Persoon();
+
+            Console.Write("Naam: ");
+            p.naam = Console.ReadLine();
+
+            Console.Write("Woonplaats: ");
+            p.woonplaats = Console.ReadLine();
+
+            Console.Write("Leeftijd: ");
+            p.leeftijd = Int32.Parse(Console.ReadLine());
+
+            return p;
+        }
+    }
+}
---
+++ week_5/Opdracht 3/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Opdracht_3
+{
+    class Program
+    {
+        static string bestandsNaam = "..\\..\\..\\2014-co2-emissions.csv";
+        static string regel = "";
+
+
+        static void Main(string[] args)
+        {
+
+            string woord = VraagWoord();
+
+            if (ZitWoordInRegel(woord, regel))
+            {
+                Console.WriteLine();
+                int aantalRijen = ZoekWoordInBestand(woord);
+                ToonWoordInRegel(woord);
+                Console.WriteLine();
+                Console.WriteLine("Aantal reels met zoekwood: " + aantalRijen);
+                Console.WriteLine("Zoekwoord komt voor op: " + regel);
+            }
+            else
+            {
+                Console.WriteLine("Helaas, niks gevonden... Type ff wat anders in ofzo!!");
+            }
+
+            Console.ReadKey();
+        }
+
+        static string VraagWoord()
+        {
+            Console.Write("Geef het zoekwoord: ");
+            string woord = Console.ReadLine();
+
+            return woord;
+
+        }
+
+        static bool ZitWoordInRegel(string woord, string regel)
+        {
+            bool woordInRegel = false;
+
+            using (StreamReader reader = File.OpenText(bestandsNaam))
+            {
+                string[] bestand = File.ReadAllLines(bestandsNaam);
+
+                for (int i = 0; i < bestand.Length - 1; i++)
+                {
+                    if(System.Text.RegularExpressions.Regex.IsMatch(bestand[i], woord, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                    //if (bestand[i].Contains("woord", StringComparison.OrdinalIgnoreCase))
+                    {
+                        woordInRegel = true;
+                    }
+                }
+            }
+
+            return woordInRegel;
+        }
+
+        static int ZoekWoordInBestand(string woord)
+        {
+            int aantalRijen = 0;
+
+            using (StreamReader reader = File.OpenText(bestandsNaam))
+            {
+                string[] bestand = File.ReadAllLines(bestandsNaam);
+
+                for (int i = 0; i < bestand.Length; i++)
+                {
+                    if (System.Text.RegularExpressions.Regex.IsMatch(bestand[i], woord, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                    //if (bestand[i].Contains("woord", StringComparison.OrdinalIgnoreCase))
+                    {
+                        aantalRijen++;
+                        regel = regel + i + ", ";
+                    }
+                }
+            }
+            return aantalRijen;
+        }
+
+        static void ToonWoordInRegel(string woord)
+        {
+            using (StreamReader reader = File.OpenText(bestandsNaam))
+            {
+                string[] bestand = File.ReadAllLines(bestandsNaam);
+
+                for (int i = 0; i < bestand.Length; i++)
+                {
+                    if (System.Text.RegularExpressions.Regex.IsMatch(bestand[i], woord, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                    //if (bestand[i].Contains("woord", StringComparison.OrdinalIgnoreCase))
+                    {
+                        int woordLocatie = bestand[i].IndexOf(woord, StringComparison.CurrentCultureIgnoreCase);
+                        string zoekWoord = bestand[i].Substring(woordLocatie);
+
+                        var punctuation = zoekWoord.Where(char.IsPunctuation).Distinct().ToArray();
+                        var words = zoekWoord.Split().Select(x => x.Trim(punctuation));
+
+                        //if (zoekWoord == words)
+                        //{
+
+                        //}
+
+                        Console.WriteLine(bestand[i]);
+                    }
+                }
+            }
+        }
+    }
+}
