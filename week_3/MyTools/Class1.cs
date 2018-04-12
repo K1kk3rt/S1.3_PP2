41a87e5ee88a64b71634e961a76dc642ad7426ae
K1kk3rt <37216006+K1kk3rt@users.noreply.github.com>
2018-04-12 13:10:04 +0200

    Add files via upload

---
+++ week_3/MyTools/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyTools
+{
+        public struct LeesTools
+        {
+            public static int LeesInt(string vraag)
+            {
+                Console.Write(vraag);
+                int getal = Int32.Parse(Console.ReadLine());
+                return getal;
+            }
+
+           public static int LeesInt(string vraag, int min, int max)
+            {
+                Console.Write(vraag);
+                int leeftijd = Int32.Parse(Console.ReadLine());
+                return leeftijd;
+            }
+
+            public static string LeesString(string vraag)
+            {
+                Console.Write(vraag);
+                string naam = Console.ReadLine();
+                return naam;
+            }
+        }
+}
---
+++ week_3/Opdracht 1_ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht_1_ConsoleApp
+{
+    using MyTools;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int getal = LeesTools.LeesInt("tik een getal: ");
+            Console.WriteLine("Je hebt {0} ingetikt.", getal);
+
+            int leeftijd = LeesTools.LeesInt("Hoe oud ben je? ", 0, 120);
+            Console.WriteLine("Je bent {0} jaar oud.", leeftijd);
+
+            string naam = LeesTools.LeesString("Hoe heet je? ");
+            Console.WriteLine("Aangenaam kennins met je te maken, {0}.", naam);
+             
+            Console.ReadKey();
+        }
+    }
+}
