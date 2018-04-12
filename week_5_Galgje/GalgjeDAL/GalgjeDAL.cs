8db067d060913edbc685ea7a824bb36afa32b899
K1kk3rt <37216006+K1kk3rt@users.noreply.github.com>
2018-04-12 13:15:08 +0200

    Add files via upload

---
+++ week_5_Galgje/GalgjeDAL/GalgjeDAL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+
+
+namespace GalgjeDAL
+{
+
+    public struct WoordenDAL
+    {
+        private static string connString = ConfigurationManager.ConnectionStrings["MyDatabaseConnection"].ConnectionString;
+        
+
+        public static string GetRandom(string bestandsNaam)
+        {
+            string woord = "";
+
+            using (StreamReader reader = File.OpenText(bestandsNaam))
+            {
+                string[] bestand = File.ReadAllLines(bestandsNaam);
+
+                Random random = new Random();
+                int index = random.Next(1, bestand.Length + 1);
+
+                woord = bestand[index];
+            }
+
+            return woord;
+        }
+
+        public static string ReadWoord(SqlDataReader reader)
+        {
+            //hulp methode voor inlezen van een woord uit een SqlDataReader
+
+            //haal gegevens op
+            string woord = (string) reader["Woord"];
+
+            return woord;
+        }
+    }
+}
---
+++ week_5_Galgje/GalgjeLogica/GalgjeLogica.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace GalgjeLogica
+{
+    using GalgjeDAL;
+    public struct Galgjespel
+    {
+        public string geheimwoord;
+        public string geradenwoord;
+
+        public void Init()
+        {
+            //method zet geradenwoord in puntjes
+            for (int i = 0; i < geheimwoord.Length; i++)
+            {
+                geradenwoord = geradenwoord + ".";
+
+            }
+
+        }
+
+        public bool RaadLetter(char Letter)
+        {
+            //method controleert of ingevoerde 'Letter' in geradenwoord zit
+
+            //maak geradenwoord Array, en zet hierin de letters van string geradenwoord
+            char[] geradenwoordArray = geradenwoord.ToCharArray();
+
+            //vergelijkt de ingevoerde letter met elke letter in geradenarray, 
+            for (int i = 0; i < geheimwoord.Length; i++)
+            {
+                if (geheimwoord[i] == (Letter))
+                {
+                    geradenwoordArray[i] = Letter;
+
+                }
+                //zet array om in string
+                geradenwoord = new string(geradenwoordArray);
+            }
+
+            //return naar GalgjeSpel()
+            if (geradenwoord.Contains(Letter.ToString()))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public bool isGeraden()
+        {
+            //method bepaalt of het hele woord geraden is
+            if (geheimwoord == geradenwoord)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public struct WoordenStruct
+    {
+
+        public static string SelecteerWoord(string bestandsNaam)
+        {
+            //WoordenDAL.GetRandom();
+            string DBwoord = WoordenDAL.GetRandom(bestandsNaam);
+
+            return DBwoord;
+        }
+    }
+}
